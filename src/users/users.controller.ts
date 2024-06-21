3d9b3afbbf65356491a943c7153aea2c2c8f2a07
Raneem <ranyseleem@gmail.com>
2024-06-21 20:00:11 +0300

    refactor(users): enhance user management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './users.service';
 import { UserDto } from './dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { OrdersService } from 'src/orders/orders.service';
 
+@ApiTags('Auth')
 @Controller('api/users')
 export class UsersController {
-  constructor(private user: UserService) {}
+  constructor(
+    private userService: UserService,
+    private orderService: OrdersService,
+  ) {}
 
-  @Post('singUp')
+  @Post('signUp')
+  @ApiResponse({ status: 201, description: 'User created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiBody({ type: UserDto, description: 'User Json details' })
   singUp(@Body() dto: UserDto) {
-    return this.user.signUp(dto);
+    return this.userService.signUp(dto);
   }
 
-  @Post('singIn')
+  @Post('signIn')
+  @ApiResponse({ status: 201, description: 'User created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiBody({ type: UserDto, description: 'User Json details' })
   singIn(@Body() dto: UserDto) {
-    return this.user.singIn(dto);
+    return this.userService.singIn(dto);
+  }
+
+  @Get(':userId/orders')
+  async getOrdersHistory(@Param('userId', new ParseIntPipe()) userId: number) {
+    return await this.orderService.getOrderHistory(userId);
   }
 }
